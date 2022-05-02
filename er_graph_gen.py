63c60414f4661ee7ce9a73b9deef5a4f92e1096e
hborkar <rishi.borkar@oracle.com>
2022-05-01 19:04:59 -0600

    Added er flags and homology

--- er_graph_gen.py
+++ er_graph_gen.py
@@ -1,18 +1,20 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-n = 10000  
-m = 0.004 * n * (n-1)  
-G = nx.gnm_random_graph(n, m)
+for i in range(6):
+    n = 1000   
+    # m = 0.004 * n * (n-1)  
+    p = 0.04
+    G = nx.erdos_renyi_graph(n, p)
 
-print("here")
+    print("here")
 
-with open("er.flag", "w") as f:
-    f.write("dim 0\n")
-    dim0 = " ".join(["0" for i in range(int(m))]) + "\n"
-    f.write(dim0)
-    f.write("dim 1\n")
-    for e in G.edges:
-        f.write(f"{e[0]} {e[1]}\n")
+    with open(f"er_graphs/er_{i}.flag", "w") as f:
+        f.write("dim 0\n")
+        dim0 = " ".join(["0" for i in range(int(n))]) + "\n"
+        f.write(dim0)
+        f.write("dim 1\n")
+        for e in G.edges:
+            f.write(f"{e[0]} {e[1]}\n")
 
 
